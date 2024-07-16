9ea8c6bcc7aa8ed916faf6bb360a82de12d6b3b3
Aqsa Imran <binteimran853@gmail.com>
2024-07-16 19:00:22 +0500

    when ShopCategory run correctly

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react'
 import "./CSS/ShopCategory.css"
+import Items from '../Components/Items/Items'
 import { ShopContext } from '../Context/ShopContext'
 const ShopCategory = (props) => {
     const {all_products}=useContext(ShopContext);
-    
+    const dataArray=all_products.filter(products=>products.category===props.category).map((dataItem,index)=>{ return <Items id={dataItem.id} name={dataItem.name} image={dataItem.image} new_price={dataItem.new_price} old_price={dataItem.old_price} />})
   return (
     <div >
 <img src={props.banner} alt="" />
 <h1>{props.category}</h1>
-<h2>{all_products}</h2>
+{dataArray}
     </div>
   )
 }
